858bcbddff49f8332eb6e66d88e94c5971cd2e03
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-09-02 23:46:11 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class pattern{
+ public static void main(String [] args){
+   int i,j,k;
+   for(i=5;i>=1;i--){
+    for(j=1;j<=i;j++)
+     System.out.print(j);
+     System.out.print("\n");
+    }
+ for(i=2;i<=5;i++){
+  for(j=1;j<=i;j++)
+   System.out.print(j);
+   System.out.print("\n");
+  }
+ }
+}
