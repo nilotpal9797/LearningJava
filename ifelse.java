a41269e1d514d0ca9161fa278997e8df7e579541
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-09-02 23:49:12 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class ifelse{
+ public static void main(String [] args ){
+   int a;
+   Scanner sc = new Scanner(System.in);
+   a=sc.nextInt();
+   if(a%2==1)
+    System.out.print("Odd\n");
+   else
+    System.out.print("Even\n");
+  }
+
+}
