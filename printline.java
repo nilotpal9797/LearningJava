cc84399bb6a63f97072739982c989ef137ee0344
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-09-02 23:52:09 +0530

    Create printline.java

---
+++ printline.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class printline{
+ public static void main(String [] args){
+   Scanner c=new Scanner(System.in);
+   String a=c.nextLine();
+   System.out.println(a);
+   }
+}
