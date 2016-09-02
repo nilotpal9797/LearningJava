0478c17a2a5eaab17d3f3eaa8df3c007717da218
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-09-02 23:42:11 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class add{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter a");
+		a=sc.nextInt();
+		System.out.println("Enter b");
+		b=sc.nextInt();
+		System.out.println("Sum is "+ (a+b));
+	}
+}
