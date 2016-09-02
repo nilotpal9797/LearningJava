7be342165c81d37dd8dfb28a74b4248da4f6f2c0
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-09-02 23:53:07 +0530

    Create inputoutput.java

---
+++ inputoutput.java
@@ -0,0 +1,10 @@
+class inputoutput{
+ public static void main(String [] args){
+      int t1=10;
+      float t2=(float)12.5;
+      String s="Java Programming";
+      System.out.println(t1);
+      System.out.println(t2);
+      System.out.println(s);
+   }  
+}
